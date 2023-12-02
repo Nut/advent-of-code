7cbddc3312e7079a88f7e9781fde3c77aa7e5848
Yannick Stephan <yannick_stephan@t-online.de>
2023-12-02 16:46:38 +0100

    Solve Day 2 - part two

--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -7,16 +7,14 @@ interface AllowedConfig {
 type Sets = AllowedConfig[];
 
 export class Day2 {
-  constructor(private allowedConfig: AllowedConfig) {}
-
-  public solve(games: string[]): number {
+  public solvePartOne(games: string[], allowedConfig: AllowedConfig): number {
     let result = 0;
 
     for (const game of games) {
       const gameId = this.getGameId(game);
       const gameSets = this.getSets(game);
 
-      if (this.isGameValid(gameSets)) {
+      if (this.isGameValid(gameSets, allowedConfig)) {
         result += gameId;
       }
     }
@@ -24,10 +22,21 @@ export class Day2 {
     return result;
   }
 
-  private isGameValid(gameSets: Sets): boolean {
+  public solvePartTwo(games: string[]): number {
+    let result = 0;
+
+    for (const game of games) {
+      const gameSets = this.getSets(game);
+      result += this.getMinPowerOfSetOfCubes(gameSets);
+    }
+
+    return result;
+  }
+
+  private isGameValid(gameSets: Sets, allowedConfig: AllowedConfig): boolean {
     for (const gameSet of gameSets) {
       for (const [key, value] of Object.entries(gameSet)) {
-        if (value > this.allowedConfig[key as keyof AllowedConfig]) {
+        if (value > allowedConfig[key as keyof AllowedConfig]) {
           return false;
         }
       }
@@ -52,5 +61,19 @@ export class Day2 {
   private getGameId(game: string): number {
     const matches = game.match(/Game \d+/g);
     return Number(matches?.[0].split(" ")[1]) || 0;
+  }
+
+  private getMinPowerOfSetOfCubes(gameSets: Sets) {
+    let maxBlue = 1;
+    let maxGreen = 1;
+    let maxRed = 1;
+
+    for (const { blue, green, red } of gameSets) {
+      maxBlue = blue > maxBlue ? blue : maxBlue;
+      maxGreen = green > maxGreen ? green : maxGreen;
+      maxRed = red > maxRed ? red : maxRed;
+    }
+
+    return maxBlue * maxGreen * maxRed;
   }
 }
--- tests/day2/day2.test.ts
+++ tests/day2/day2.test.ts
@@ -2,25 +2,51 @@ import { readFileSync } from "fs";
 import { Day2 } from "~/day2/day2";
 
 describe("Day2", () => {
-  test("should solve simple example", () => {
-    const file = readFileSync("./tests/day2/day2_test_input.txt", "utf-8");
-    const input = file.split("\n");
-    const allowedConfig = { red: 12, green: 13, blue: 14 };
-    const day2 = new Day2(allowedConfig);
+  describe("part one", () => {
+    test("should solve simple example", () => {
+      const file = readFileSync("./tests/day2/day2_test_input.txt", "utf-8");
+      const input = file.split("\n");
+      const allowedConfig = { red: 12, green: 13, blue: 14 };
+      const day2 = new Day2();
 
-    const result = day2.solve(input);
+      const result = day2.solvePartOne(input, allowedConfig);
 
-    expect(result).toBe(8);
+      expect(result).toBe(8);
+    });
+
+    test("should solve test example", () => {
+      const file = readFileSync("./tests/day2/day2_input.txt", "utf-8");
+      const input = file.split("\n");
+      const allowedConfig = { red: 12, green: 13, blue: 14 };
+      const day2 = new Day2();
+
+      const result = day2.solvePartOne(input, allowedConfig);
+
+      console.log("Result of Day 2 - part one: ", result);
+      expect(result).toBe(2551);
+    });
   });
 
-  test("should solve test example", () => {
-    const file = readFileSync("./tests/day2/day2_input.txt", "utf-8");
-    const input = file.split("\n");
-    const allowedConfig = { red: 12, green: 13, blue: 14 };
-    const day2 = new Day2(allowedConfig);
+  describe("part two", () => {
+    test("should solve simple example", () => {
+      const file = readFileSync("./tests/day2/day2_test_input.txt", "utf-8");
+      const input = file.split("\n");
+      const day2 = new Day2();
 
-    const result = day2.solve(input);
+      const result = day2.solvePartTwo(input);
 
-    console.log("Result of Day 2 - part one: ", result);
+      expect(result).toBe(2286);
+    });
+
+    test("should solve test example", () => {
+      const file = readFileSync("./tests/day2/day2_input.txt", "utf-8");
+      const input = file.split("\n");
+      const day2 = new Day2();
+
+      const result = day2.solvePartTwo(input);
+
+      console.log("Result of Day 2 - part two: ", result);
+      expect(result).toBe(62811);
+    });
   });
 });
